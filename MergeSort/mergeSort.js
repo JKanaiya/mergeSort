1116b59f3b3f56e3c31f38e2782e2d268b58aedb
JKanaiya <JKanaiya>
2025-11-18 14:38:14 +0300

    Feat: README and cli funciton call functionality

--- MergeSort/mergeSort.js
+++ MergeSort/mergeSort.js
@@ -1,3 +1,5 @@
+const process = require("process");
+
 const merge = function (nArr, mArr) {
   let merged = [];
   let tot = nArr.length + mArr.length;
@@ -25,8 +27,13 @@ const mergeSort = function (arr) {
   if (arr.length <= 1) return arr;
   return merge(
     mergeSort(arr.slice(0, arr.length / 2)),
-    mergeSort(arr.slice(arr.length / 2))
+    mergeSort(arr.slice(arr.length / 2)),
   );
 };
 
-console.log(mergeSort([1, 4, 6, 8, 2, 3, 5, 7, 9]));
+if (process.argv[2]) {
+  const arr = process.argv[2].split(",");
+  console.log(mergeSort(arr));
+} else {
+  console.log(mergeSort([1, 4, 6, 8, 2, 3, 5, 7, 9]));
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# MergeSort
+
+MergeSort provides a recursive solution to sort the numbers given.
+
+## Usage
+
+1. Clone the repo
+```bash
+git clone https://github.com/JKanaiya/mergeSort
+cd mergeSort/MergeSort
+```
+2. Install dependencies
+```bash
+npm install
+```
+
+4. Sort numbers given 
+```bash
+# Give the numbers eg node mergeSort.js 1,4,5,3,6,1
+node mergeSort.js num1,num2,num3 ... num
+```
+
+
+Features
+--------
+
+- Displays the sorted numbers in an array 
+
+License
+-------
+
+The project is licensed under the BSD license.
+
